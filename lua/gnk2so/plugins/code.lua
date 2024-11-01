d2c4384296abd84c8e7e7f46c2f1ee38372a9da1
Gabriel Neves <gabriel.santos@labldi.com.br>
2024-11-01 07:25:44 -0300

    feat: add auto-complete plugin

--- lua/gnk2so/plugins/code.lua
+++ lua/gnk2so/plugins/code.lua
@@ -1,5 +1,30 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = {
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end
+  },
+  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
@@ -11,7 +36,19 @@ return {
       require("mason-lspconfig").setup({
         ensured_installed = {
           "jdtls",
+          "lua_ls"
         }
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup {
+            capabilities = capabilities,
+          }
+        end,
       })
     end
   },
