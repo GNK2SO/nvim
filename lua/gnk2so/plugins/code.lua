f6b0e3b035729f98a756915290e21b47b2d44099
Gabriel Neves <gabriel.santos@labldi.com.br>
2024-11-03 09:18:23 -0300

    refactor: extract auto-complete plugin to his own file

--- lua/gnk2so/plugins/code.lua
+++ lua/gnk2so/plugins/code.lua
@@ -1,30 +1,5 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = {
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-      })
-    end
-  },
-  {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
