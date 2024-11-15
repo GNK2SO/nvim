7d32e9a03a4d9afe12e70c026dd7d4f6ba68f2f1
Gabriel Neves <gabriel.santos@labldi.com.br>
2024-11-15 12:50:06 -0300

    feat: add highlight config for js, ts, C and lua languages

--- lua/gnk2so/plugins/tree-sitter.lua
+++ lua/gnk2so/plugins/tree-sitter.lua
@@ -3,7 +3,7 @@ return {
   run = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "java" },
+      ensure_installed = { "c", "lua","java", "javascript", "typescript" },
       highlight = { enable = true },
     })
   end
