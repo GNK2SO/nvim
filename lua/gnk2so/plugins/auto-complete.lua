c876bac52595950d221fac9fc02e5aa84253aa3f
Gabriel Neves <gabriel.santos@labldi.com.br>
2024-11-21 20:24:21 -0300

    feat: add snippet engine plugin for nvim-cmp

--- lua/gnk2so/plugins/auto-complete.lua
+++ lua/gnk2so/plugins/auto-complete.lua
@@ -5,23 +5,56 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      luasnip.setup({
+        history = true,
+        updateevents = "TextChanged, TextChangedI",
+      })
+
       cmp.setup({
+        snippet = {
+          expand = function (args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
         mapping = {
           ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.close(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         },
         sources = {
           { name = "nvim_lsp" },
           { name = "buffer" },
           { name = "path" },
+          { name = "luasnip" },
         },
       })
     end
-  }
+  },
 }
